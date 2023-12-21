fa920c78b72521ee2039e12415b5a7b855dd8088
hetame1 <poi8946@naver.com>
2023-12-22 02:00:46 +0900

    12/22

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,13 +1,68 @@
-import { StatusBar } from "expo-status-bar";
-import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import * as Location from "expo-location";
+import { Ionicons } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "75ff5ec496fd50a633332ba4b619861f";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "sunny",
+  Atmosphere: "hail",
+  Snow: "snowy",
+  Rain: "rainy",
+  Drizzle: "hail",
+  Thunderstorm: "lightning",
+};
+
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+
+    setCity(location[0].city);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+
+    const json = await response.json();
+
+    setDays(json.list);
+  };
+
+  useEffect(() => {
+    getWeather();
+  });
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
@@ -16,22 +71,42 @@ export default function App() {
         horizontal
         style={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="black"
+              size="large"
+              style={{
+                marginTop: 10,
+              }}
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between",
+                  paddingHorizontal: 20,
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.main.temp).toFixed(1)}
+                </Text>
+                <Ionicons
+                  name={icons[day.weather[0].main]}
+                  size={60}
+                  color="black"
+                  style={{ marginTop: 50 }}
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -54,14 +129,14 @@ const styles = StyleSheet.create({
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 158,
   },
   description: {
     marginTop: -30,
     fontSize: 60,
+    paddingLeft: 20,
   },
 });
