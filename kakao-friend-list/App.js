79addd911cebe0413c7d51b1c940be0e99352744
hetame1 <poi8946@naver.com>
2023-12-26 03:46:18 +0900

    12/26 study
    성능 개선을 위해 FlatList 공부

--- kakao-friend-list/App.js
+++ kakao-friend-list/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import Header from "./src/Header";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
@@ -8,48 +8,127 @@ import Margin from "./src/Margin";
 import Division from "./src/Division";
 import FriendSection from "./src/FriendSection";
 import FriendList from "./src/FriendList";
+import { useState } from "react";
+import TabBar from "./src/TabBar";
 
 const statusBarHeight = getStatusBarHeight();
 
 export default function App() {
+  const [isOpened, setIsOpened] = useState(true);
+  const [selectedTabIdx, setSelectedTabIdx] = useState(0);
+
   const onPressArrow = () => {
-    console.log("Arrow pressed");
+    setIsOpened(!isOpened);
   };
+
+  const ItemSeparatorComponent = () => <Margin height={13} />;
+
+  const renderItem = ({ item }) => (
+    <View>
+      <Profile
+        uri={item.uri}
+        name={item.name}
+        introduction={item.introduction}
+        isMe={false}
+      />
+    </View>
+  );
+
+  const ListHeaderComponent = () => (
+    <View style={{ backgroundColor: "white" }}>
+      <Header />
+
+      <Margin height={10} />
+
+      <Profile
+        uri={myProfile.uri}
+        name={myProfile.name}
+        introduction={myProfile.introduction}
+        isMe={true}
+      />
+
+      <Margin height={15} />
+
+      <Division />
+
+      <Margin height={12} />
+
+      <FriendSection
+        friendProfileLen={friendProfiles.length}
+        onPress={onPressArrow}
+        isOpened={isOpened}
+      />
+
+      <Margin height={5} />
+    </View>
+  );
+
+  const ListFooterComponent = () => <Margin height={10} />;
 
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <Header />
-
-        <Margin height={10} />
-
-        <Profile
-          uri={myProfile.uri}
-          name={myProfile.name}
-          introduction={myProfile.introduction}
+        <FlatList
+          data={isOpened ? friendProfiles : []}
+          contentContainerStyle={{ paddingHorizontal: 15 }}
+          keyExtractor={(_, index) => index}
+          stickyHeaderIndices={[0]}
+          ItemSeparatorComponent={ItemSeparatorComponent}
+          renderItem={renderItem}
+          ListHeaderComponent={ListHeaderComponent}
+          ListFooterComponent={ListFooterComponent}
+          showsVerticalScrollIndicator={false}
         />
 
-        <Margin height={15} />
-
-        <Division />
-
-        <Margin height={12} />
-
-        <FriendSection
-          friendProfileLen={friendProfiles.length}
-          onPressArrow={onPressArrow}
+        <TabBar
+          selectedTabIdx={selectedTabIdx}
+          setSelectedTabIdx={setSelectedTabIdx}
         />
-
-        <FriendList data={friendProfiles} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
+
+  // return (
+  //   <SafeAreaProvider>
+  //     <SafeAreaView style={styles.container}>
+  //       <View style={{ flex: 1, paddingHorizontal: 15 }}>
+  // <Header />
+
+  // <Margin height={10} />
+
+  // <Profile
+  //   uri={myProfile.uri}
+  //   name={myProfile.name}
+  //   introduction={myProfile.introduction}
+  // />
+
+  // <Margin height={15} />
+
+  // <Division />
+
+  // <Margin height={12} />
+
+  // <FriendSection
+  //   friendProfileLen={friendProfiles.length}
+  //   onPress={onPressArrow}
+  //   isOpened={isOpened}
+  // />
+
+  //         <FriendList data={friendProfiles} isOpened={isOpened} />
+  //       </View>
+
+  //       <TabBar
+  //         selectedTabIdx={selectedTabIdx}
+  //         setSelectedTabIdx={setSelectedTabIdx}
+  //       />
+  //     </SafeAreaView>
+  //   </SafeAreaProvider>
+  // );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingHorizontal: 15,
   },
 });
