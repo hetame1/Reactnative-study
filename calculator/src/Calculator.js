fd49e6a58115b6bfe0a63e0a03f2a457c40e6e1c
hetame1 <poi8946@naver.com>
2023-12-27 03:37:54 +0900

    12/27 study
    패스트캠퍼스 react-native 강의 입문 ch02 init

---
+++ calculator/src/Calculator.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TouchableOpacity, View } from "react-native";
+
+const Button = ({ text, onPress, flex, type }) => {
+  return <TouchableOpacity style={{}}></TouchableOpacity>;
+};
+
+const Calculator = () => {
+  return <View></View>;
+};
+
+export default Calculator;
