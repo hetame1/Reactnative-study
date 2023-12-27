ec38d98071765909dde45a2ee59528c78bac68dc
hetame1 <poi8946@naver.com>
2023-12-28 02:36:42 +0900

    12/28 study
    마진 따로 뺴내기

---
+++ todo-calendar/src/Margin.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+
+const Margin = ({ height }) => {
+  return (
+    <View
+      style={{
+        height,
+      }}
+    />
+  );
+};
+
+export default Margin;
