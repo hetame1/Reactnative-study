6a37da5eb8c15b633e0922763c2705aa0c7013cb
hetame1 <poi8946@naver.com>
2024-01-02 16:42:48 +0900

    1/2 study
    todolist 추가

---
+++ todo-calendar/src/hook/use-todo-list.js
@@ -0,0 +1,76 @@
+import dayjs from "dayjs";
+import React, { useState } from "react";
+
+const defaultTodoList = [
+  {
+    id: 1,
+    content: "Buy milk",
+    date: dayjs(),
+    isSuccess: true,
+  },
+  {
+    id: 2,
+    content: "Do homework",
+    date: dayjs(),
+    isSuccess: false,
+  },
+  {
+    id: 3,
+    content: "Read book",
+    date: dayjs(),
+    isSuccess: false,
+  },
+];
+
+const useTodoList = (selectedDate) => {
+  const [todoList, setTodoList] = useState(defaultTodoList);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    const len = todoList.length;
+    const lastId = len === 0 ? 0 : todoList[len - 1].id;
+
+    const newTodoList = [
+      ...todoList,
+      {
+        id: lastId + 1,
+        content: input,
+        date: selectedDate,
+        isSuccess: false,
+      },
+    ];
+
+    setTodoList(newTodoList);
+  };
+
+  const removeTodo = (id) => {
+    const newTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(newTodoList);
+  };
+
+  const toggleTodo = (id) => {
+    const newTodoList = todoList.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          isSuccess: !todo.isSuccess,
+        };
+      }
+
+      return todo;
+    });
+
+    setTodoList(newTodoList);
+  };
+
+  return {
+    todoList,
+    input,
+    setInput,
+    addTodo,
+    removeTodo,
+    toggleTodo,
+  };
+};
+
+export default useTodoList;
